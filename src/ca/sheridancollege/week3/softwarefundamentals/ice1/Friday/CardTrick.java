7a8f8f9cad9baea08b13f8daab87dd7aaffde7ff
joshua1 <fortesra@sheridancollege.ca>
2021-02-14 01:01:10 -0500

    Added setSuit & setValue into the main class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,12 +1,27 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joshua Student 
+ * ID: 991598983 
+ * Student Name: Raphael Joshua Fortes
+ */
 
 public class CardTrick {
+
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Card[] myHand = new Card[4];
         
-        Card c = new Card();
-        
-        System.out.println(c.getValue());
-        System.out.println(c.getSuit());
+        for(int i = 0; i < myHand.length; i++) {
+            Card c = new Card(); 
+            
+            c.setSuit(Card.SUITS[(int)((Math.random()*4)+0)]);
+            c.setValue((int)((Math.random()*13)+1));
+
+            
+            myHand[i] = c;
+        }
     }
-    
 }
