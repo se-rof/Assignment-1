dca3b1d885badac63b45a43b71ec67b4b5fa4919
joshua1 <fortesra@sheridancollege.ca>
2021-02-13 22:16:22 -0500

    Create RaphaelJoshua_Fortes_ICE1  branch

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,9 +1,12 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 public class CardTrick {
-    public static void main(String[] args)
-    {
-       
+    public static void main(String[] args) {
+        
+        Card c = new Card();
+        
+        System.out.println(c.getValue());
+        System.out.println(c.getSuit());
     }
     
 }
